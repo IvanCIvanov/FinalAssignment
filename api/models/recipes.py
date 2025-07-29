2ff2bfaeaf0cc133bcc01ee559a7aeda49c9bd15
grishmahowale <grishmahowale@gmail.com>
2025-07-28 22:12:49 -0400

    Revisions made for  models for recipes and menu items with proper foreign keys

--- api/models/recipes.py
+++ api/models/recipes.py
@@ -7,9 +7,9 @@ from ..dependencies.database import Base
 class Recipe(Base):
     __tablename__ = "recipes"
 
-    recipe_id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    recipe_id = Column("id",Integer, primary_key=True, index=True, autoincrement=True)
     sandwich_id = Column(Integer, ForeignKey("menu_items.sandwich_id"))
-    ingredient_id = Column(Integer, ForeignKey("resources.ingredient_id"))
+    ingredient_id = Column("resource_id", Integer, ForeignKey("resources.ingredient_id"))
 
     menu_item = relationship("MenuItem", back_populates="recipe")
     ingredient = relationship("Resource", back_populates="recipes")
